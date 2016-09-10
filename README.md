a7c31babe4fe355f83789b8460684b36bbbef1b8
Dino Trojak <dinotrojak@gmail.com>
2016-09-10 12:51:55 +0200

    added cursor color changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Reactor homepage
+# [Reactor](http://reactor.studio) homepage
 #### Deployment
 `scp -r ./dist/* USER@IP:/usr/share/nginx/html/reactor`
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,20 +5,49 @@ import "styles/reactor.styl";
 import React from 'react';
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+      num: 1
+    }
+    this.changeColors = this.changeColors.bind(this);
+  }
+  genNumber() {
+    return Math.floor((Math.random() * 3) + 1);
+  }
+  changeColors() {
+    let color = "red"
+    let num = this.genNumber()
+    while(this.state.num === num) {
+      num = this.genNumber()
+    }
+    if(num === 2) {
+      color = "white"
+    } else if (num === 3) {
+      color = "black"
+    }
+    this.setState({ color, num });
+  }
   render() {
     return (
-      <div className="reactor">
-        <div className="content">
-          <div className="logo">
+      <div
+        className={`wraper ${this.state.color}`}
+        onClick={this.changeColors}
+      >
+        <div className="reactor">
+          <div className="content">
+            <div className="logo">
+            </div>
+            <div className="text">
+              <h2>Reactor is a new digital product studio in Hong Kong and Zagreb
+                that creates joyful digital ideas, products and experiences. <br/> They connect
+                hearts of brands with hearts of their consumers.</h2>
+              <div className="line"></div>
+              <p className="contact"><b>Contact Us</b> <br/> hello@reactor.studio</p>
+            </div>
+            <p className="copyright">© 2016  ReactorStudio Limited</p>
           </div>
-          <div className="text">
-            <h2>Reactor is a new digital product studio in Hong Kong and Zagreb
-              that creates joyful digital ideas, products and experiences. <br/> They connect
-              hearts of brands with hearts of their consumers.</h2>
-            <div className="line"></div>
-            <p className="contact"><b>Contact Us</b> <br/> hello@reactor.studio</p>
-          </div>
-          <p className="copyright">© 2016  ReactorStudio Limited</p>
         </div>
       </div>
     );
