e8b3bcc81b020651758d545513bbb218560f76d9
Dino Trojak <dino.trojak@kiwi.com>
2020-02-04 14:51:15 +0100

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@reactor3`,
   },
   plugins: [
+    'gatsby-plugin-styled-components',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
